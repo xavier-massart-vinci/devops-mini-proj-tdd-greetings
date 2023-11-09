c2a15da06634c95e5f432131907e2a8bc74f5a66
Chisom <chisom.ubah@student.vinci.be>
2023-11-09 22:06:03 +0100

    add : case with Maj and min names

--- src/app.js
+++ src/app.js
@@ -1,6 +1,46 @@
 class Main {
-    greet(name) {
-        return `Hello, ${name}.`;
+    greet(names) {
+
+        if (typeof names == "string") return `Hello, ${names}.`; 
+
+
+        if (typeof names == "object") {
+            
+            let resultat = `Hello, `;
+            const namesMaj = [];
+            const namesMin = [];
+
+            for (let index = 0; index < names.length; index++) {
+                let element = names[index];
+                let firstLetterOut = element.slice(1, 2);
+                (firstLetterOut === firstLetterOut.toLowerCase()) ? namesMin.push(element) : namesMaj.push(element);
+            }
+
+            resultat += `${namesMin[0]}`;
+
+            for (let index = 1; index < namesMin.length; index++) {
+                resultat +=  ` and ${namesMin[index]}`;
+
+                if (index + 1 === namesMin.length) resultat += '.';
+            
+            }
+            if (1 === namesMin.length) resultat += '.';
+
+            
+            if (namesMin.length!==0) {
+                resultat += ` AND `;
+            }
+            resultat += `HELLO ${namesMaj[0]}`;
+
+            for (let index = 1; index < namesMaj.length; index++) {
+                resultat += ` AND ${namesMaj[index]}`;
+            
+                if (index + 1 === namesMaj.length) resultat +='!';
+            }
+            if (1 === namesMaj.length) resultat += '!';
+            return resultat;
+            console.log(resultat);
+        }
     }
 }
 module.exports = Main;
--- test/app.test.js
+++ test/app.test.js
@@ -13,4 +13,11 @@ test('giving bob shoud be Hello, bob.', () => {
 
 });
 
+test('giving “KRATOS”, “Thanathos”,“Hypnos”', () => {
+
+    let resultat = main.greet(['KRATOS','Thanathos','Hypnos']);
+
+    expect(resultat).toBe('Hello, Thanathos and Hypnos. AND HELLO KRATOS!');
+  
+});
 
