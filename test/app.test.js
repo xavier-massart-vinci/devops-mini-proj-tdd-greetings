ccaf7fdcf79b36f2b53fcd46db66ba81258e91ad
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-11-11 19:11:03 +0100

    test: change naming + variable set to const

--- test/app.test.js
+++ test/app.test.js
@@ -1,55 +1,55 @@
 const Main = require('../src/app');
 
 //Arrange 
-let main = new Main; 
+const main = new Main; 
 
 test('giving bob shoud be Hello, bob.', () => {
 
     //Act
-    let resultat = main.greet('bob');
+    const result = main.greet('bob');
 
     //Assert 
-    expect(resultat).toBe('Hello, bob.');
+    expect(result).toBe('Hello, bob.');
 
 });
 
 test('giving null shoud be Hello, my friend.', () => {
 
     //Act
-    let resultat = main.greet(null);
+    const result = main.greet(null);
 
     //Assert 
-    expect(resultat).toBe('Hello, my friend.');
+    expect(result).toBe('Hello, my friend.');
 
 });
 
 test('giving undefined shoud be Hello, my friend.', () => {
 
     //Act
-    let resultat = main.greet(undefined);
+    const result = main.greet(undefined);
 
     //Assert 
-    expect(resultat).toBe('Hello, my friend.');
+    expect(result).toBe('Hello, my friend.');
 
 });
 
 test('giving an empty string shoud be Hello, my friend.', () => {
 
     //Act
-    let resultat = main.greet("   ");
+    const result = main.greet("   ");
 
     //Assert 
-    expect(resultat).toBe('Hello, my friend.');
+    expect(result).toBe('Hello, my friend.');
 
 });
 
 test('giving bob in capital letters shoud be HELLO, BOB!', () => {
 
     //Act
-    let resultat = main.greet("BOB");
+    const result = main.greet("BOB");
 
     //Assert 
-    expect(resultat).toBe('HELLO, BOB!');
+    expect(result).toBe('HELLO, BOB!');
 
 });
 
