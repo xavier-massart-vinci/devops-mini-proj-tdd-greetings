63d3cfd13c644bb770290a71ef35eee33940b959
Chisom <chisom.ubah@student.vinci.be>
2023-11-12 11:57:35 +0100

    refactor(app.js): add 2 function that handle each an array

--- src/app.js
+++ src/app.js
@@ -10,48 +10,40 @@ class Main {
   }
 
   multiNames(names) {
-    let majNames = [];
-    let minNames = [];
+    let namesMaj = [];
+    let namesMin = [];
     let resultat = '';
 
-    for (let i = 0; i < names.length; i++) {
-      if (names[i].slice(1, 2) === names[i].slice(1, 2).toUpperCase())
-        majNames.push(names[i]);
-      else {
-        minNames.push(names[i]);
-      }
+    names.forEach((name) => {
+      name.slice(1, 2) === name.slice(1, 2).toUpperCase()
+        ? namesMaj.push(name)
+        : namesMin.push(name);
+    });
+
+    if (namesMin.length > 0) resultat += this.minNames(namesMin);
+    if (namesMaj.length > 0 && namesMin.length > 0) resultat += ' AND ';
+    if (namesMaj.length > 0) resultat += this.majNames(namesMaj);
+    return resultat;
+  }
+
+  minNames(namesMin) {
+    let resultat = `Hello, ${namesMin[0]}`;
+
+    for (let index = 1; index < namesMin.length - 1; index++) {
+      resultat += `, ${namesMin[index]}`;
     }
+    resultat +=
+      namesMin.length > 1 ? ` and ${namesMin[namesMin.length - 1]}.` : '.';
+    return resultat;
+  }
 
-    if (minNames.length > 0) {
-      resultat = `Hello, ${minNames[0]}`;
-
-      for (let index = 1; index < minNames.length - 1; index++) {
-        let element = minNames[index];
-        resultat += `, ${element}`;
-      }
-
-      if (minNames.length > 1) {
-        resultat += ` and ${minNames[minNames.length - 1]}.`;
-      } else {
-        resultat += '.';
-      }
+  majNames(namesMaj) {
+    let resultat = `HELLO ${namesMaj[0]}`;
+    for (let index = 1; index < namesMaj.length - 1; index++) {
+      resultat += `, ${namesMaj[index]}`;
     }
-
-    if (majNames.length > 0 && minNames.length > 0) resultat += ' AND ';
-
-    if (majNames.length > 0) {
-      resultat += `HELLO ${majNames[0]}`;
-      for (let index = 1; index < majNames.length - 1; index++) {
-        let element = majNames[index];
-        resultat += `, ${element}`;
-      }
-      if (majNames.length > 1) {
-        resultat += ` AND ${majNames[majNames.length - 1]}!`;
-      } else {
-        resultat += `!`;
-      }
-    }
-
+    resultat +=
+      namesMaj.length > 1 ? ` AND ${namesMaj[namesMaj.length - 1]}!` : `!`;
     return resultat;
   }
 }
